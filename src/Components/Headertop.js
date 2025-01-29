f33c1b1e0079c4018a599368da1745ddb6dbe409
adilraees786 <adilraees911@gmail.com>
2025-01-29 16:38:31 +0500

    set icons

--- src/Components/Headertop.js
+++ src/Components/Headertop.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import { FaFacebook } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
+import { FaFacebook,FaTwitter , FaInstagram,FaLinkedin } from "react-icons/fa";
+
 
 const Headertop = () => {
     return (
