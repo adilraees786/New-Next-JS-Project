12c6f19d4a908eec2d5ea8ea83446a1c109fc31d
adilraees786 <adilraees911@gmail.com>
2025-01-30 15:14:20 +0500

    start Navbar

---
+++ src/Components/Navbar..js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className=' hidden lg:block '>
+      <div className=' container'>
+        <div className='flex '>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Headertop from "@/Components/Headertop";
 import Headermain from "@/Components/Headermain";
+import Navbar from "@/Components/Navbar.";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({ children }) {
         
          <Headertop/>
          <Headermain/>
+         <Navbar/>
         {children}
       </body>
     </html>
