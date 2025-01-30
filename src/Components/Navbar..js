94bf49b0b1248ce04cc51e0c70c94d3ca3ef514a
adilraees786 <adilraees911@gmail.com>
2025-01-30 15:27:03 +0500

    set navbar link styling

--- src/Components/Navbar..js
+++ src/Components/Navbar..js
@@ -1,11 +1,16 @@
+import Link from 'next/link';
 import React from 'react'
+
 
 const Navbar = () => {
   return (
     <div className=' hidden lg:block '>
       <div className=' container'>
-        <div className='flex '>
-
+        <div className='flex  w-fit gap-10 mx-auto font-medium py-4 text-blackish'>
+<Link className=" navbar__link relative" 
+href="#">
+  Home
+  </Link>
         </div>
       </div>
     </div>
