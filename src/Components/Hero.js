a4b728fb4d9a808c1e5eba286a8959c7ab230113
adilraees786 <adilraees911@gmail.com>
2025-01-30 16:58:10 +0500

    add slide data

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,4 +1,10 @@
+"use client";
+
 import React from 'react'
+import Slider from 'react-slick';
+import Slide from './Slide';
+
+
 
 
 const Hero = () => {
@@ -11,19 +17,52 @@ const Hero = () => {
         pauseOnHover: false,
      };
 
-     const sliderData =[
+     const slideData =[
         {
             id:1,
-            img:"",
-            title: "",
-            mainTitle: "",
+            img:"/banner-1.jpg",
+            title: "Trading Item",
+            mainTitle: "WOMEN'S LATEST FASHION SALE",
             price:"$20"
 
-        }
+        },
+        {
+            id:2,
+            img:"/banner-2.jpg",
+            title: "Trading Accessories",
+            mainTitle: "MODERN SUNGLASSES",
+            price:"$15"
+
+        },
+        {
+            id:3,
+            img:"/banner-3.jpg",
+            title: "SALE OFFER",
+            mainTitle: "NEW FASHION SUMMER SALE",
+            price:"$30"
+
+        },
      ]
 
   return (
-    <div>Hero</div>
+    <div>
+        <div className=' container pt-6 lg:pt-0'>
+            <Slider {...setting}>
+                {slideData.map((item) => (
+                <Slide 
+                key={item.id}
+                img={item.img}
+                title={item.title}
+                mainTitle={item.mainTitle}
+                price={item.price}
+                />
+                 ))}
+
+            </Slider>
+
+        </div>
+
+    </div>
   )
 }
 
