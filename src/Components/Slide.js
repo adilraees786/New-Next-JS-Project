14d3bbd16a6814aed0240c2b1dbc4f1785fca3ed
adilraees786 <adilraees911@gmail.com>
2025-01-31 16:34:07 +0500

    add new div in slide for shop now

--- src/Components/Slide.js
+++ src/Components/Slide.js
@@ -28,31 +28,33 @@
 
 
 import React from 'react';
-const Slide= (img, title, mainTitle,price) => {
-  return (
-    <div className=' outline-none border-none relative '>
-        <div className=' absolute left-[30px] md:left-[70px]
+const Slide = (img, title, mainTitle, price) => {
+    return (
+        <div className=' outline-none border-none relative '>
+            <div className=' absolute left-[30px] md:left-[70px]
          max-w-[250px] sm:max-w-[350px] top-[50%] 
          -translate-y-[50%] space-y-2 lg:space-y-4
           bg-[#ffffffa2] sm:bg-transparent p-4
            sm:p-0 rounded-lg sm:rounded-none'>
-            <h3 className=' text-accent text-[24px] lg:text-[28px]'>{title}</h3>
-            <h2 className=' text-blackish text-[26px] md:text-[30px] lg:text-[44px]'>{mainTitle}</h2>
-<h3 className=' text-[24px] text-gray-500 '>
-    Starting at
-    <b className=' text-[20px] md:text-[24px] lg:text-[30px]'>
-        {price}
-    </b>
-</h3>
-<div className=''>
+                <h3 className=' text-accent text-[24px] lg:text-[28px]'>{title}</h3>
+                <h2 className=' text-blackish text-[26px] md:text-[30px] lg:text-[44px]'>{mainTitle}</h2>
+                <h3 className=' text-[24px] text-gray-500 '>
+                    Starting at
+                    <b className=' text-[20px] md:text-[24px] lg:text-[30px]'>
+                        {price}
+                        .00
+                    </b>
+                </h3>
+                <div className=' bg-accent text-white text-[14px] md:text-[16px] p-2 px-4
+                 rounded-lg inline-block cursor-pointer hover:bg-black'>
+                    SHOP NOW
+                </div>
 
-</div>
 
+            </div>
 
         </div>
-
-    </div>
-  )
+    )
 }
 
 export default Slide
