41cbf723e16016b50e10a573bac1375ca6f42222
adilraees786 <adilraees911@gmail.com>
2025-02-01 15:24:58 +0500

    ADD image style

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Image from 'next/image'
 
-const ProductCard = () => {
-  return (
-    <div>ProductCard</div>
-  )
+const ProductCard = (img, title, desc, rating, price) => {
+    return (
+        <div className='px-4 border border-gray-200 rounded-xl max-w-[400px]'>
+            <div className=''>
+                <Image className=" w-full h-auto"
+                src={img}
+                width={200}
+                heigt={300}
+                alt={title}
+                 />
+            </div>
+        </div>
+    )
 }
 
 export default ProductCard
