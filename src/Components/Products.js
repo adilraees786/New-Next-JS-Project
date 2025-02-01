93e1a8c591af7a84b8fcad28cc7922f874109fbc
adilraees786 <adilraees911@gmail.com>
2025-02-01 15:01:45 +0500

    add product data

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,5 +1,64 @@
 import React from 'react'
 
+ const Productdata=[
+    {
+        img: "/jacket.jpg",
+        title: "JACKET",
+        desc:"MEN Yarn Fleece Full-Zip Jacket",
+        rating: 5,
+        price: "58.00"
+
+    },
+    {
+        img: "/skirt-1.jpg",
+        title: "SKIRT",
+        desc:"Black Floral Wrap Midi Skirt",
+        rating: 4,
+        price: "80.00"
+
+    },
+    {
+        img: "/party-wear-1.jpg",
+        title: "Party Wear",
+        desc:"Women's Party Wear Shoes",
+        rating: 3,
+        price: "28.00"
+
+    },
+    {
+        img: "/shirt-1.jpg",
+        title: "SHIRT",
+        desc:"Pure Garment Dyed Cotton Shirt",
+        rating: 4,
+        price: "55.00"
+
+    },
+    {
+        img: "/sports-1.jpg",
+        title: "SPORTS",
+        desc:"Trekking & Running Shoes - Black",
+        rating: 4,
+        price: "68.00"
+
+    },
+    {
+        img: "/watch-1.jpg",
+        title: "Watches",
+        desc:"Sports Watches Vital Plus",
+        rating: 4,
+        price: "100.00"
+
+    },
+    {
+        img: "/watch-2.jpg",
+        title: "Watches",
+        desc:"Pocket Watch Leather Pouch",
+        rating: 4,
+        price: "145.00"
+
+    },
+   
+ ]
 
 
 const Products = () => {
