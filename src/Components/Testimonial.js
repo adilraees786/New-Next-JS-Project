2e2250846b3bc69daf21e8560b2389e9c0a53dfd
adilraees786 <adilraees911@gmail.com>
2025-02-01 16:44:11 +0500

    add data in tesitimonial

--- src/Components/Testimonial.js
+++ src/Components/Testimonial.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Testimonial = () => {
-  return (
-    <div>Testimonial</div>
-  )
+    return (
+        <div>
+            <div className=' container pt-16 pb-16 grid-cols-2 '>
+                <h2 className=' font-medium text-2xl pb-4'>Testimonial</h2>
+                <div className=' grid lg:grid-cols-[300px, 1fr] gap-4'>
+                    <div className=' border border-gray-300 rounded-2xl grid place-items-center p-6 lg:p-0'>
+                        <div className=' text-center flex flex-col items-center gap-1'>
+                            <Image
+                                className="rounded-full inline-block"
+                                src="/user.jpg"
+                                width={80}
+                                height={80}
+                                alt="dp"
+                            />
+                            <h2 className=' text-gray-500 font-black text-[20px'>
+                                Chota Don
+                            </h2>
+                            <p>CEO & Fonder Invision</p>
+                        <image className=' inline-block py-2'
+                        
+                        src="/quotes.svg"
+                        width={50}
+                        height={50}
+                        alt="quotes"
+                        />
+                        <p className=' max-w-[200px] text-gray-500'>
+                           Chota don
+
+
+
+                        </p>
+                        
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Testimonial
