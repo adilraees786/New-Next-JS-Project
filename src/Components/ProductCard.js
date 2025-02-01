4edf727fec704cba913d2bd16cb67367b9e1b14e
adilraees786 <adilraees911@gmail.com>
2025-02-01 16:16:21 +0500

    set price tag

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -30,7 +30,10 @@ const ProductCard = ({ img, title, desc, rating, price }) => {
                 <p className='text-gray-500 max-w-[150px]'>{desc}</p>
                 <div>{generateRating(rating)}</div>
                 <div className='font-bold flex gap-4 text-lg text-green-600'>
-                    ${price} <del className='text-gray-400'>$100</del>
+                    ${price} 
+                    <del className='text-gray-400'>
+                        ${parseInt(price) + 50}.00
+                    </del>
                 </div>
             </div>
         </div>
