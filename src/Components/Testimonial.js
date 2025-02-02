6ce89f15143ecefd66e55410d8587b934150d7ec
adilraees786 <adilraees911@gmail.com>
2025-02-02 19:33:02 +0500

    set side div

--- src/Components/Testimonial.js
+++ src/Components/Testimonial.js
@@ -20,26 +20,37 @@ const Testimonial = () => {
                                 Chota Don
                             </h2>
                             <p>CEO & Fonder Invision</p>
-                        <image className=' inline-block py-2'
-                        
-                        src="/quotes.svg"
-                        width={50}
-                        height={50}
-                        alt="quotes"
-                        />
-                        <p className=' max-w-[200px] text-gray-500'>
-                        "E-commerce shopping" refers to the act of buying and selling goods or services online,
-                         essentially meaning you purchase items through a digital platform like a website or
-                          mobile app, instead of visiting a physical store; it's short for "electronic commerce".
-                          
+                            <image className=' inline-block py-2'
 
+                                src="/quotes.svg"
+                                width={50}
+                                height={50}
+                                alt="quotes"
+                            />
+                            <p className=' max-w-[200px] text-gray-500'>
+                                "E-commerce shopping" refers to the act of buying and selling goods or services online,
+                                essentially meaning you purchase items through a digital platform like a website or
+                                mobile app, instead of visiting a physical store; it's short for "electronic commerce".
+                            </p>
 
-
-                        </p>
-                        
                         </div>
                     </div>
 
+                    <div className=' bg-red-600 bg-[url(/summer.jpg)] bg-cover h-[480px] rounded-2xl
+                                      place-items-center'>
+                        <div className=' bg-[#ffffffab] min-w-[270px] sm:min-w-[300px] md:min-w-[500px]
+                                          rounded-xl py-8 sm:px-8 px-2 grid place-items-center gap-3'>
+                            <button className=' bg-blackish text-white p-2 rounded-md'>
+                                30% DISCOUNT
+                            </button>
+                            <h2 className=' font-extrabold text-2xl  text-[#272727]'>
+                                Summer Collection
+                            </h2>
+                            <p className=' text-gray-500 text-[20px]'>
+                                Starting @ $20 <b>Shop Now</b>
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
